010198e26163bbed9952b5d1b31bbb9188577012
Vero <james.asghar@gmail.com>
2020-03-14 15:21:08 -0500

    Finished first tutorial Delay was not working, so switched to using close button

--- fourier-drawing/main.c
+++ fourier-drawing/main.c
@@ -7,3 +7,62 @@
 //
 
 #include <stdio.h>
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+typedef enum {FALSE = 0 , TRUE = 1} bool;
+
+
+int main(int argc, char* argv[]) {
+    
+    // The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    // The surface contained by the window
+    SDL_Surface* screen_surface = NULL;
+    
+    
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not be initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        // Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if(window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            // Get window surface
+            screen_surface = SDL_GetWindowSurface(window);
+            
+            // Fill the surface white
+            SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+            
+            // Update the surface
+            SDL_UpdateWindowSurface(window);
+            
+            // Blocks window from closing
+            int is_running = TRUE;
+            SDL_Event event;
+            while (is_running) {
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) {
+                        is_running = FALSE;
+                    }
+                }
+                SDL_Delay(16);
+            }
+
+        }
+    }
+    
+    // Destroy window
+    SDL_DestroyWindow(window);
+    
+    // Quit SDL subsystems
+    SDL_Quit();
+    
+    return 0;
+}
